83095a10fbdd7eeab8f7acd43d54f687c7d806a2
Arjo <arjo.mahuya@gmail.com>
2026-08-20 16:07:44 +0800

    update name to Alfred

---
+++ src/main/java/Alfred.java
@@ -0,0 +1,10 @@
+public class Alfred {
+    public static void main(String[] args) {
+        String banner = "    _    _  __              _\n"
+                + "   / \\  | |/ _|_ __ ___  __| |\n"
+                + "  / _ \\ | | |_| '__/ _ \\/ _` |\n"
+                + " / ___ \\| |  _| | |  __/ (_| |\n"
+                + "/_/   \\_\\_|_| |_|  \\___|\\__,_|\n";
+        System.out.println(banner);
+    }
+}
