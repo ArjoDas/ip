00514792dacff9a2bd72e374e75e338e92afc141
Arjo <arjo.mahuya@gmail.com>
2026-08-20 16:10:41 +0800

    add greeting

--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -1,10 +1,26 @@
+/**
+ * Entry point for the Alfred chatbot.
+ * Prints a greeting and immediately exits.
+ */
 public class Alfred {
+    /** Horizontal divider used to frame chatbot messages. */
+    private static final String LINE =
+            "____________________________________________________________";
+
+    private static final String BANNER =
+            "     _    _     _____ ____  _____ ____  \n"
+                    + "    / \\  | |   |  ___|  _ \\| ____|  _ \\ \n"
+                    + "   / _ \\ | |   | |_  | |_) |  _| | | | |\n"
+                    + "  / ___ \\| |___|  _| |  _ <| |___| |_| |\n"
+                    + " /_/   \\_\\_____|_|   |_| \\_\\_____|____/";
+
     public static void main(String[] args) {
-        String banner = "    _    _  __              _\n"
-                + "   / \\  | |/ _|_ __ ___  __| |\n"
-                + "  / _ \\ | | |_| '__/ _ \\/ _` |\n"
-                + " / ___ \\| |  _| | |  __/ (_| |\n"
-                + "/_/   \\_\\_|_| |_|  \\___|\\__,_|\n";
-        System.out.println(banner);
+        System.out.println(LINE);
+        System.out.println(BANNER);
+        System.out.println("Hello! I'm Alfred.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
