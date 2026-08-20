7d2156dfffae318d85288381f551811b5d4d545a
Arjo <arjo.mahuya@gmail.com>
2026-08-20 16:26:07 +0800

    add echo

--- README.md
+++ README.md
@@ -13,19 +13,29 @@ Prerequisites: JDK 25, update Intellij to the most recent version.
    1. If there are any further prompts, accept the defaults.
 1. Configure the project to use **JDK 25** (not other versions) as explained in [here](https://www.jetbrains.com/help/idea/sdk.html#set-up-jdk).<br>
    In the same dialog, set the **Project language level** field to the `SDK default` option.
-1. After that, locate the `src/main/java/Alfred.java` file, right-click it, and choose `Run Alfred.main()` (if the code editor is showing compile errors, try restarting the IDE). If the setup is correct, you should see something like the below as the output:
+1. After that, locate the `src/main/java/Alfred.java` file, right-click it, and choose `Run Alfred.main()` (if the code editor is showing compile errors, try restarting the IDE). If the setup is correct, you should see a greeting. Alfred then echoes each command you type, and exits when you type `bye`:
    ```
-   ____________________________________________________________
-       _    _  __              _
-      / \  | |/ _|_ __ ___  __| |
-     / _ \ | | |_| '__/ _ \/ _` |
-    / ___ \| |  _| | |  __/ (_| |
-   /_/   \_\_|_| |_|  \___|\__,_|
-   Hello! I'm Alfred.
-   What can I do for you?
-   ____________________________________________________________
-   Bye. Hope to see you again soon!
-   ____________________________________________________________
+       ____________________________________________________________
+           _    _  __              _
+          / \  | |/ _|_ __ ___  __| |
+         / _ \ | | |_| '__/ _ \/ _` |
+        / ___ \| |  _| | |  __/ (_| |
+       /_/   \_\_|_| |_|  \___|\__,_|
+        Hello! I'm Alfred.
+        What can I do for you?
+       ____________________________________________________________
+   list
+       ____________________________________________________________
+        list
+       ____________________________________________________________
+   blah
+       ____________________________________________________________
+        blah
+       ____________________________________________________________
+   bye
+       ____________________________________________________________
+        Bye. Hope to see you again soon!
+       ____________________________________________________________
    ```
 
 **Warning:** Keep the `src\main\java` folder as the root folder for Java files (i.e., don't rename those folders or move Java files to another folder outside of this folder path), as this is the default location some tools (e.g., Gradle) expect to find Java files.
--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -1,26 +1,66 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Alfred chatbot.
- * Prints a greeting and immediately exits.
+ * Greets the user, echoes each command, and exits on {@code bye}.
  */
 public class Alfred {
     /** Horizontal divider used to frame chatbot messages. */
     private static final String LINE =
-            "____________________________________________________________";
+            "    ____________________________________________________________";
+
+    /** Indent applied to chatbot message text. */
+    private static final String INDENT = "     ";
 
     private static final String BANNER =
-            "    _    _  __              _\n"
-                    + "   / \\  | |/ _|_ __ ___  __| |\n"
-                    + "  / _ \\ | | |_| '__/ _ \\/ _` |\n"
-                    + " / ___ \\| |  _| | |  __/ (_| |\n"
-                    + "/_/   \\_\\_|_| |_|  \\___|\\__,_|";
+            "        _    _  __              _\n"
+                    + "       / \\  | |/ _|_ __ ___  __| |\n"
+                    + "      / _ \\ | | |_| '__/ _ \\/ _` |\n"
+                    + "     / ___ \\| |  _| | |  __/ (_| |\n"
+                    + "    /_/   \\_\\_|_| |_|  \\___|\\__,_|";
 
     public static void main(String[] args) {
-        System.out.println(LINE);
+        greet();
+        echoUntilBye();
+    }
+
+    /** Prints the welcome banner and opening prompt. */
+    private static void greet() {
+        printLine();
         System.out.println(BANNER);
-        System.out.println("Hello! I'm Alfred.");
-        System.out.println("What can I do for you?");
+        printMessage("Hello! I'm Alfred.");
+        printMessage("What can I do for you?");
+        printLine();
+    }
+
+    /**
+     * Reads user commands from standard input, echoes each one,
+     * and stops when the user types {@code bye}.
+     */
+    private static void echoUntilBye() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                showReply("Bye. Hope to see you again soon!");
+                break;
+            }
+            showReply(command);
+        }
+    }
+
+    /** Frames a single chatbot reply between divider lines. */
+    private static void showReply(String message) {
+        printLine();
+        printMessage(message);
+        printLine();
+    }
+
+    private static void printLine() {
         System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(INDENT + message);
     }
 }
